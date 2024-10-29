a6ce17e6b68b9346c025bf90236285d2bde2cd67
1thenameisshivam <1thenameisshivam@gmail.com>
2024-10-29 23:46:16 +0530

    getUserBookedSeat controller created

--- src/Booking/routes/booking.routes.js
+++ src/Booking/routes/booking.routes.js
@@ -3,9 +3,12 @@ import { lockSeats } from "../controller/seatLocking.controller.js";
 import isAuthenticated from "../../middleware/isAuthenticated.js";
 import { getSessionId } from "../controller/getSessionId.controller.js";
 import { varifyPayment } from "../controller/varifyPayment.controller.js";
+import { getUserBookedSeats } from "../controller/getUserBookedSeats.js";
+import limiter from "../../middleware/rateLimit.js";
 const bookingRouter = express.Router();
 
-bookingRouter.post("/lock_seat", isAuthenticated, lockSeats);
-bookingRouter.post("/get_session_id", isAuthenticated, getSessionId);
-bookingRouter.post("/varify", isAuthenticated, varifyPayment);
+bookingRouter.post("/lock_seat", limiter, isAuthenticated, lockSeats);
+bookingRouter.post("/get_session_id", limiter, isAuthenticated, getSessionId);
+bookingRouter.post("/varify", limiter, isAuthenticated, varifyPayment);
+bookingRouter.get("/booked_seats", isAuthenticated, getUserBookedSeats);
 export default bookingRouter;
