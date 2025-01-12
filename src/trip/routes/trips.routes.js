46ba2e6f8c23f14f527abfd37c2f7a684487d15e
1thenameisshivam <1thenameisshivam@gmail.com>
2025-01-12 16:04:43 +0530

    testing

--- src/trip/routes/trips.routes.js
+++ src/trip/routes/trips.routes.js
@@ -6,7 +6,7 @@ import isAuthorised from "../../middleware/isAuthorised.js";
 import isAuthenticated from "../../middleware/isAuthenticated.js";
 import { getTrips } from "../controller/getTrips.controller.js";
 import { singleTrip } from "../controller/singleTrip.js";
-import {viewTrip} from "../controller/viewTrip.js";
+import { viewTrip } from "../controller/viewTrip.js";
 import { deleteTrip } from "../controller/deleteTrip.js";
 const tripRouter = express.Router();
 tripRouter.post(
@@ -14,12 +14,14 @@ tripRouter.post(
   [isAuthenticated, isAuthorised, upload.single("image")],
   createTrip
 );
+
 tripRouter.patch(
   "/editTrip/:id",
   [isAuthenticated, isAuthorised, upload.single("image")],
   editTrip
 );
-tripRouter.get("/viewTrip/:id",isAuthenticated, viewTrip);
+
+tripRouter.get("/viewTrip/:id", isAuthenticated, viewTrip);
 tripRouter.get("/", getTrips);
 tripRouter.get("/:id", singleTrip);
 tripRouter.delete("/delete/:id", [isAuthenticated, isAuthorised], deleteTrip);
